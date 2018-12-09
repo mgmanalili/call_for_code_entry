0032e2c43f5292aa77380302751030cf051ff02b
Michael Andrew Manalili <michaelandrew.manalili@gmail.com>
2018-12-09 18:48:54 +0100

    gdacs parse with wfp iso3

--- sandbox_geoparse.py
+++ sandbox_geoparse.py
@@ -1,61 +1,83 @@
 import feedparser
-import pandas as pd
+import requests
 import matplotlib.pyplot as plt
-import geopandas as gpd
+import pandas as pd
 from geopandas import GeoDataFrame
 from shapely.geometry import Point
 import folium
 import os
 %matplotlib notebook
 
-### GeoRSS
+### GDACS GeoRSS
 GDACS = 'http://www.gdacs.org/xml/rss.xml'
-COPERNICUS_RAPID_MAPPING = 'http://emergency.copernicus.eu/mapping/activations-rapid/feed'
-EONET = 'https://eonet.sci.gsfc.nasa.gov/api/v2.1/events/rss.xml'
-NOAA_TSUNAMI = 'https://ptwc.weather.gov/feeds/ptwc_rss_pacific.xml'
 
-RSS_URLS = [GDACS, NOAA_TSUNAMI, COPERNICUS_RAPID_MAPPING, EONET]
+RSS_URL = [GDACS]
 
-feeds = []
+feed = []
+iso3 = []
+country = []
+etype = []
+epid = []
+evid = []
 guid = []
-xy = []
-title = []
-desc = []
-link = []
+pubdate = []
+bbox = []
+coords = []
+alevel = []
+ascore = []
 
-for url in RSS_URLS:
-    feeds.append(feedparser.parse(url))
+wfpiso3 = ['CHL', 'IDN', 'PHL', 'SDN', 'SSD', 'DRC', 'BGD', 'CAF', 'BWA', 'TCD', 'KHM']
 
-for feed in feeds:
-    for post in feed.entries:
-        guid.append(post.guid)
-        xy.append(post.where)
-        title.append(post.title)
-        desc.append(post.description)
-        link.append(post.link)
+def get_data():
+    for url in RSS_URL:
+        feeds.append(feedparser.parse(url))
+
+    for feed in feeds:
+        for post in feed.entries:
+            iso3.append(post.gdacs_iso3)
+            country.append(post.gdacs_country)
+            etype.append(post.gdacs_eventtype)
+            epid.append(post.gdacs_episodeid)
+            evid.append(post.gdacs_eventid)
+            guid.append(post.guid)
+            pubdate.append(post.published)
+            bbox.append(post.gdacs_bbox)
+            coords.append(post.where)
+            alevel.append(post.gdacs_episodealertlevel)
+            ascore.append(post.gdacs_alertscore)
+            
+        guiddf = pd.DataFrame(guid)       #0
+        pubdatedf = pd.DataFrame(pubdate) #1
+        isodf = pd.DataFrame(iso3)        #2   
+        countrydf = pd.DataFrame(country) #3
+        etypedf = pd.DataFrame(etype)     #4
+        epidf = pd.DataFrame(epid)        #5
+        evidf = pd.DataFrame(evid)        #6
+        bboxdf = pd.DataFrame(bbox)       #7
+        coordf = pd.DataFrame(coords)     #8 #9
+        aleveldf = pd.DataFrame(alevel)   #10
+        ascoredf = pd.DataFrame(ascore)   #11
         
-    #print len(guid, title, coords)
-    df_guid['guid'] = pd.DataFrame(guid)
-    df_xy = pd.DataFrame(xy)
-    df_title['title'] = pd.DataFrame(title)
-    df_desc['desc'] = pd.DataFrame(desc)
-    df_link['link'] = pd.DataFrame(link)
-    
-    frames = [df_guid['guid'], df_xy, df_title['title'], df_desc['desc'], df_link['link']]
-    
-    data = pd.concat([df_guid['guid'], df_xy, df_title['title'], df_desc['desc'], df_link['link']],axis=1, ignore_index=False)
-data.head(2)
+        frames = [guiddf, pubdatedf, isodf, countrydf, etypedf, epidf, evidf, bboxdf,coordf, aleveldf, ascoredf]
+        df = pd.concat(frames, axis=1, ignore_index = True, names=[frames])
+        return df[df[2].isin(wfpiso3)] #!IMPORTANT!#
+        #return df
+        
+
+data = get_data()
+data
 #f = 'C:/Users/Michael/Desktop/Notebooks/test.csv'
 #data.to_csv(f, encoding='utf-8')  
 
-geometry = [Point(xy) for xy in zip(data['coordinates'])]
-df = data.drop(['coordinates'], axis=1)
+geometry = [Point(xy) for xy in zip(data[8])]
+gdacsdf = data.drop([8], axis=1)
 crs = {'init': 'epsg:4326'}
-gdf = GeoDataFrame(df, crs=crs, geometry=geometry)
-gdf.head(3)
+gdf = GeoDataFrame(gdacsdf, crs=crs, geometry=geometry)
+gdf.head()
 
-geodata = gpd.GeoDataFrame(gdf)
-geodata.head(3)
+#Same as line 75/56
+#geodata = gpd.GeoDataFrame(gdf)
+#geodata.head(3)
 
 jsondata = geodata.to_json()
 
